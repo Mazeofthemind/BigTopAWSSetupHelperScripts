fc7e59dd25ed2d08f357dafb75001d76af71bc25
Cloud User <centos@ip-10-0-0-62.ec2.internal>
2016-01-08 17:05:21 +0000

    Added BigTop dependencies to the local repo formation, along with a .yum templating segment

--- BigTopLocalRepoMaker.sh
+++ BigTopLocalRepoMaker.sh
@@ -1,7 +1,11 @@
-#Assumes you are in a temporary directory which does not contain an archive.zip file
+####Downloads the current versions of Apache BigTop packages from Jenkins and places in a local directory to be converted into a local repository
+#Note: Script assumes you are in a writable directory which does not contain an archive.zip file
 
+echo Downloading current versions of Apache BigTop packages from Jenkins to make local repo 
 BIGTOP_COMPONENTS=("spark", "hadoop", "hue", "zeppelin", "pig", "zookeeper", "bigtop-groovy", "bigtop-jsvc", "bigtop-tomcat", "bigtop-utils")
+BIGTOP_DEPENDENCIES=("puppet", "java-1.8.0-openjdk") 
 OUTPUT_REPO="/home/centos/localrepo/"
+GATEWAY_FQDN=ip-10-0-0-62
 
 for COMPONENT in ${BIGTOP_COMPONENTS[*]}
 do
@@ -10,3 +14,23 @@ do
 	rm archive.zip
 done
 
+####Downloads the packages which are dependencies for Apache Bigtop (along with their dependencies) and places them in a local directory to be conerted into a local repository
+echo Downloading current version of Apache Bigtop dependencies to make local repo
+for DEPENDENCY in ${BIGTOP_DEPENDENCIES[*]}
+do
+	repotrack -a x86_64 -p $OUTPUT_REPO $DEPENDENCY
+done
+
+####Creates a local repository from the local download directory
+echo Building local repository 
+createrepo --database $OUTPUT_REPO
+
+####Creates the .yum repo file from template
+cat >~/localrepo/BigTopLocalRepo.yum <<EOF
+
+[BigTop-Local]
+name=Bigtop Local Repository
+baseurl=http://$GATEWAY_FQDN:8000/
+gpgcheck=0
+
+EOF
